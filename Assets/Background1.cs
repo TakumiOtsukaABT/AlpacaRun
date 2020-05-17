23b331622ee10029188aad280d8352f9c78c9112
Takumi0310 <takumiotsuka2@yahoo.com>
2020-05-18 01:25:31 +0900

    add infinite scrolling background

---
+++ Assets/Background1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background1 : MonoBehaviour
+{
+    public float scrollSpeed = -5f;
+    public Vector2 startPos;
+    const float imageBound = 22.455f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, imageBound);
+        transform.position = startPos + Vector2.right * newPos;
+    }
+}
