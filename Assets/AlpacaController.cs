f3c832b0ff75bea4b8115a1f8eebdb1c74f7e087
Takumi0310 <takumiotsuka2@yahoo.com>
2020-06-07 20:26:41 +0900

    implement jump by space

---
+++ Assets/AlpacaController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlpacaController : MonoBehaviour
+{
+    Rigidbody2D rig;
+    public float force = 680.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rig.AddForce(transform.up * this.force);
+        }
+
+
+    }
+}
