9a63e5e5d34434a3584211b6ba9a67e3f140d74a
Takumi0310 <takumiotsuka2@yahoo.com>
2020-06-08 00:36:38 +0900

    Add four direction detection

--- Assets/AlpacaController.cs
+++ Assets/AlpacaController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AlpacaController : MonoBehaviour
 {
     Rigidbody2D rig;
-    public float force = 680.0f;
-    // Start is called before the first frame update
+    public float force = 1000.0f;
+    Vector2 startPos;
     void Start()
     {
         this.rig = GetComponent<Rigidbody2D>();
@@ -16,11 +17,52 @@ public class AlpacaController : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
-            this.rig.AddForce(transform.up * this.force);
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            Vector2 result = endPos - startPos;
+            switch (fourDirection(result))
+            {
+                case Direction.UP:
+                    Debug.Log("UP");
+                    this.rig.AddForce(transform.up * this.force);
+                    break;
+                case Direction.DOWN:
+                    Debug.Log("DOWN");
+                    break;
+                case Direction.RIGHT:
+                    Debug.Log("RIGHT");
+                    break;
+                case Direction.LEFT:
+                    Debug.Log("LEFT");
+                    break;
+            }
         }
 
 
     }
+    enum Direction
+    {
+        UP,
+        DOWN,
+        RIGHT,
+        LEFT
+    }
+
+    private Direction fourDirection(Vector2 vector2)
+    {
+        if (Mathf.Abs(vector2.y) > Mathf.Abs(vector2.x))
+        {
+            return (vector2.y > 0) ? Direction.UP : Direction.DOWN;
+        }
+        else {
+            return (vector2.x > 0) ? Direction.RIGHT : Direction.LEFT;
+        }
+    }
+
+
 }
