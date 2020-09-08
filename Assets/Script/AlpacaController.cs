4423d9339a64ea6ebcd06f7f2829d87a82f69e65
Takumi-styler <Takumi.Otsuka@styler.link>
2020-09-08 19:51:16 +0900

    add korobi

--- Assets/Script/AlpacaController.cs
+++ Assets/Script/AlpacaController.cs
@@ -17,8 +17,12 @@ public class AlpacaController : MonoBehaviour
     float seconds = 0;
     public GameObject shougeki;
     Vector2 tempPos;
+    bool korobiBool = false;
 
     Animator animator;
+    public float korobiTime;
+    public float speedAdjust;
+
     void Start()
     {
         Application.targetFrameRate = 30;
@@ -33,50 +37,53 @@ public class AlpacaController : MonoBehaviour
     {
         if (Mathf.Approximately(Time.timeScale, 0f)||disable)
         {
+            this.transform.Translate((-this.speed / speedAdjust) * (Time.deltaTime * 60), 0, 0);
             return;
         }
         this.animator.speed = speed * (Time.deltaTime * 60);
-        if (Input.GetMouseButtonDown(0))
+        if (state != 3)
         {
-            this.startPos = Input.mousePosition;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            Vector2 endPos = Input.mousePosition;
-            Vector2 result = endPos - startPos;
-            switch (fourDirection(result))
+            if (Input.GetMouseButtonDown(0))
             {
-                case Direction.UP:
-                    Debug.Log("UP");
-                    if(rig.velocity.y == 0&&state==0)
-                    {
-                        this.rig.AddForce(transform.up * this.force);
-                    }
-                    break;
-                case Direction.DOWN:
-                    Debug.Log("DOWN");
-                    if (state == 0 && rig.velocity.y == 0)
-                    {
-                        state = 1;
-                    }
-                    break;
-                case Direction.RIGHT:
-                    Debug.Log("RIGHT");
-                    if (state == 0)
-                    {
-                        GameObject go = Instantiate(shougeki) as GameObject;
-                        go.transform.parent = this.transform;
-                        go.transform.localPosition = new Vector3(-1.0f, 0, 0);
-                        tempPos = transform.position;
-                        state = 2;
-                    }
-                    break;
-                case Direction.LEFT:
-                    Debug.Log("LEFT");
-                    break;
+                this.startPos = Input.mousePosition;
             }
-        }//input
-
+            else if (Input.GetMouseButtonUp(0))
+            {
+                Vector2 endPos = Input.mousePosition;
+                Vector2 result = endPos - startPos;
+                switch (fourDirection(result))
+                {
+                    case Direction.UP:
+                        Debug.Log("UP");
+                        if (rig.velocity.y == 0 && state == 0)
+                        {
+                            this.rig.AddForce(transform.up * this.force);
+                        }
+                        break;
+                    case Direction.DOWN:
+                        Debug.Log("DOWN");
+                        if (state == 0 && rig.velocity.y == 0)
+                        {
+                            state = 1;
+                        }
+                        break;
+                    case Direction.RIGHT:
+                        Debug.Log("RIGHT");
+                        if (state == 0)
+                        {
+                            GameObject go = Instantiate(shougeki) as GameObject;
+                            go.transform.parent = this.transform;
+                            go.transform.localPosition = new Vector3(-1.0f, 0, 0);
+                            tempPos = transform.position;
+                            state = 2;
+                        }
+                        break;
+                    case Direction.LEFT:
+                        Debug.Log("LEFT");
+                        break;
+                }
+            }//input
+        }
         switch (state)
         {
             case 0://walk or jump
@@ -85,8 +92,11 @@ public class AlpacaController : MonoBehaviour
                 {
                     this.animator.SetBool("Ground", false);
                 }
-                else
+                else if(this.korobiBool)
                 {
+                    state = 3;
+                }
+                else {
                     this.animator.SetBool("Ground", true);
                 }
                 break;
@@ -112,35 +122,37 @@ public class AlpacaController : MonoBehaviour
                     tackleOnFinished();
                 }
                 break;
+            case 3:
+                seconds += Time.deltaTime;
+                if (seconds < korobiTime)
+                {
+                    korobi();
+                }
+                else if (seconds > korobiTime)
+                {
+                    korobiOnFinished();
+                }
+                break;
 
         }
         this.animator.SetInteger("State", state);
 
+    }
 
+    private void korobiOnFinished()
+    {
+        Debug.Log("korobifin");
+        speed = 1.0f;
+        {
+            state = 0;
+            seconds = 0;
+            korobiBool = false;
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private void korobi()
+    {
+        this.transform.Translate((-this.speed / speedAdjust) * (Time.deltaTime * 60), 0, 0);
     }
 
     private void duckOnFinished()
@@ -195,6 +207,23 @@ public class AlpacaController : MonoBehaviour
         {
             state = 0;
             seconds = 0;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Background1>())
+        {
+            return;
+        }
+        if (collision.gameObject.GetComponent<Enemy>())
+        {
+            Debug.Log("cast success");
+            if (rig.velocity.y <= 0)
+            {
+                korobiBool = true;
+            }
+            
         }
     }
 
